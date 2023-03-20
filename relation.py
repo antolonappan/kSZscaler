8b14a2ab5afa45c79372993afc796409faf2a468
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-03-20 14:00:57 +0100

    fit
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- relation.py
+++ relation.py
@@ -3,6 +3,7 @@ import numpy as np
 from astropy import units as u
 import astropy.constants as c
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 class Scaling:
     
@@ -23,11 +24,37 @@ class Scaling:
         second = np.abs(np.array(self.dataframe['vx[km/s]'])) * (u.km/u.s)
         third = (self.Mgas * u.M_sun).to('kg') / (c.m_p)
         return (first.to('cm s') * second.to('cm/s') * third).to('Mpc^2')
+
+    def gas_halo_relation(self) -> None:
+        Mhalo = np.array(self.dataframe['m500c[Msol/h]']) /self.h * u.M_sun
+        Mgas = self.Mgas * u.M_sun
+        plt.scatter(Mgas,Mhalo,s=0.3)
+        plt.semilogy()
+        plt.semilogx()
+        plt.xlabel(f'Mgas (${Mgas.unit}$)')
+        plt.ylabel(f'Mhalo (${Mhalo.unit}$)')
+        plt.title('Mgas vs Mhalo')
+        plt.show()
+    
+    def velo_halo_relation(self,ax) -> None:
+        Mhalo = np.array(self.dataframe['m500c[Msol/h]']) /self.h * u.M_sun
+        Vgas = np.abs(np.array(self.dataframe[f'v{ax}[km/s]'])) * (u.km/u.s)
+        plt.scatter(Vgas,Mhalo,s=0.3)
+        plt.semilogy()
+        #plt.semilogx()
+        plt.xlabel(f'Vgas (${Vgas.unit}$)')
+        plt.ylabel(f'Mhalo (${Mhalo.unit}$)')
+        plt.title('Vgas vs Mhalo')
+        plt.show()
     
 
-    def Y_M(self) ->None:
+    def Y_M(self) -> tuple:
         M = np.array(self.dataframe['m500c[Msol/h]']) /self.h * u.M_sun
         Y = self.Yksz()
+        return (Y,M)
+    
+    def plot_Y_M(self) -> None:
+        Y,M = self.Y_M()
         plt.scatter(M,Y,s=0.3)
         plt.semilogy()
         plt.semilogx()
@@ -35,6 +62,32 @@ class Scaling:
         plt.ylabel(f'Yksz (${Y.unit}$)')
         plt.title('Yksz vs M500c')
         plt.show()
+
+    def power_law(self,x,a,b):
+        return a*x**b
+    
+    def Y_M_fit(self) -> tuple:
+        Y,M = self.Y_M()
+        xdata_log = np.log(M.value)
+        ydata_log = np.log(Y.value)
+        popt, pcov = curve_fit(self.power_law, xdata_log, ydata_log)
+        yfit_log = self.power_law(xdata_log,*popt)
+        return (popt,pcov,np.exp(yfit_log))
+    
+    def plot_Y_M_fit(self) -> None:
+        Y,M = self.Y_M()
+        popt,pcov,yfit_log = self.Y_M_fit()
+        plt.scatter(M,Y,s=0.3)
+        plt.semilogy()
+        plt.semilogx()
+        plt.xlabel(f'Mgas (${M.unit}$)')
+        plt.ylabel(f'Yksz (${Y.unit}$)')
+        plt.title('Yksz vs M500c')
+        plt.plot(M,yfit_log,label=f"$\\alpha$={popt[1]:.2f}",c='r')
+        plt.legend()
+    
+
+
 
 
     
