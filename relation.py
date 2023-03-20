d52a197cc31f97d06eda73444f55e890dc036fdc
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-03-20 05:56:29 +0100

    units
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- relation.py
+++ relation.py
@@ -16,15 +16,23 @@ class Scaling:
     @property
     def Mgas(self) -> np.ndarray:
         return np.array(self.dataframe['gas_frac'].values * \
-                        self.dataframe['m500c[Msol/h]'] * self.h)
+                        self.dataframe['m500c[Msol/h]'] / self.h)
     
     def Yksz(self) -> np.ndarray:
-        return (c.sigma_T/c.c) * (self.dataframe['vx[km/s]'] *1e3) * (self.Mgas/(c.m_p))
+        first = c.sigma_T/c.c
+        second = np.abs(np.array(self.dataframe['vx[km/s]'])) * (u.km/u.s)
+        third = (self.Mgas * u.M_sun).to('kg') / (c.m_p)
+        return (first.to('cm s') * second.to('cm/s') * third).to('Mpc^2')
+    
 
     def Y_M(self) ->None:
-        plt.scatter(np.array(self.dataframe['m500c[Msol/h]']),self.Yksz(),s=0.3)
-        plt.xlabel('Mgas')
-        plt.ylabel('Yksz')
+        M = np.array(self.dataframe['m500c[Msol/h]']) /self.h * u.M_sun
+        Y = self.Yksz()
+        plt.scatter(M,Y,s=0.3)
+        plt.semilogy()
+        plt.semilogx()
+        plt.xlabel(f'Mgas (${M.unit}$)')
+        plt.ylabel(f'Yksz (${Y.unit}$)')
         plt.title('Yksz vs M500c')
         plt.show()
 
